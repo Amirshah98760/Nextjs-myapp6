d195a5a325971abc4ed8f6aa10ff19ccd7e04ecc
Amirshah98760 <amirshah98760@gmail.com>
2026-02-23 12:34:54 +0500

    Add the client component code also

--- app/datafetch/clientComp/page.jsx
+++ app/datafetch/clientComp/page.jsx
@@ -1,20 +1,23 @@
 "use client"
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import { useSearchParams } from 'next/navigation'
 
 const clientComp = () => {
+    const [userInfo , setUserInfo] = useState({});
  const searchParams = useSearchParams();
  const name = searchParams.get("name");
  console.log(name)
 
 
-    const dataFetch = async () => {
+ 
+
+    useEffect(() => {
+           const dataFetch = async () => {
         const res = await fetch(`https://api.genderize.io/?name=${name}`);
         const data = await res.json();
         console.log(data);
+        setUserInfo(data);
     }
-
-    useEffect(() => {
         dataFetch();
     }, []);
 
@@ -22,9 +25,13 @@ const clientComp = () => {
 
 
   return (
-    <div className='flex h-screen items-center justify-center'>
+    <div className='flex h-screen items-center justify-center flex-col gap-2'>
         <h1 className='text-2xl font-bold'>clientComp</h1>
-        
+        <p className='text-lg font-semibold'>{userInfo.name}</p>
+        <p className='text-lg font-semibold'>{userInfo.gender}</p>
+        <p className='text-lg font-semibold'>{userInfo.probability}</p>
+
+        {/* <div className="bg-gradient-to-br from-blue-500 to-purple-600 h-4 w-full"></div> */}
     </div>
   )
 }
